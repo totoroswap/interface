5632127d0c285e9babbf81630e500e20a898000e
totoroswap <totoroswap@aol.com>
2021-10-12 14:46:19 +0800

    sign name

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -123,7 +123,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' }
     ]
     const domain = {
-      name: 'TotoroSwap Liquidity Provider Token',
+      name: 'TotoroSwap.com Liquidity Provider Token',
       version: '1',
       chainId: chainId,
       verifyingContract: pair.liquidityToken.address
