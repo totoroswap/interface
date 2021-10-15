3c87666e14dd41c635db59043da6e06289af785e
totoro_03 <03_totoroswap@aol.com>
2021-10-15 15:51:30 +0800

    layout menu

--- src/components/SidebarMenu/index.tsx
+++ src/components/SidebarMenu/index.tsx
@@ -1,26 +1,63 @@
-import React from 'react'
-import ExchangeIcon from '../../assets/svg/menu_line.svg'
+import React, { useState } from 'react'
+import ExchangeIcon from '../../assets/svg/tokenlist.svg'
 
-import { SidebarMenuView } from './style'
+import { SidebarMenuView, MenuItem } from './style'
+import { Moon, Sun, Globe } from 'react-feather'
+import { useDarkModeManager } from '../../state/user/hooks'
+import { LANGUAGES } from '../../i18n'
+import { useTranslation } from 'react-i18next'
+
+const sidebarMenuList = [
+  {
+    icon: ExchangeIcon,
+    name: 'Exchange',
+    route: '/swap'
+  },
+  {
+    icon: ExchangeIcon,
+    name: 'Liquidity',
+    route: '/pool'
+  }
+]
 export default function SidebarMenu() {
-  const sidebarMenuList = [
-    {
-      icon: ExchangeIcon,
-      name: 'Exchange',
-      router: '/swap'
-    }
-  ]
+  const [darkMode, toggleDarkMode] = useDarkModeManager()
+  const { i18n } = useTranslation()
+  const [showLanguage, setShowLanguage] = useState(false)
+  const languageCheck = LANGUAGES.find(i => i.key === i18n.language)
+  const changeLanguage = (key: string) => {
+    setShowLanguage(false)
+    i18n.changeLanguage(key)
+  }
+
   return (
     <SidebarMenuView>
       <div className="menu-list">
         {sidebarMenuList.map((item, index) => (
-          <div className="menu-item" key={index}>
+          <MenuItem key={index} to={item.route}>
             <img src={item.icon} alt={item.name} />
             <span>{item.name}</span>
-          </div>
+          </MenuItem>
         ))}
       </div>
-      <div className="menu-footer"></div>
+      <div className="menu-footer">
+        <div className="theme-switch-btn" onClick={toggleDarkMode}>
+          {darkMode ? <Moon size={20} /> : <Sun size={20} />}
+          {darkMode ? 'Light Theme' : 'Dark Theme'}
+        </div>
+        <div className="language-switch-btn" onClick={() => setShowLanguage(!showLanguage)}>
+          <Globe size={19} />
+          {languageCheck && languageCheck.name}
+          {showLanguage && (
+            <div className="language-list">
+              {LANGUAGES.map(item => (
+                <div className="language-item" onClick={() => changeLanguage(item.key)}>
+                  {item.name}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </SidebarMenuView>
   )
 }
--- src/components/SidebarMenu/style.tsx
+++ src/components/SidebarMenu/style.tsx
@@ -1,27 +1,60 @@
 import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
 export const SidebarMenuView = styled.div`
   width: 240px;
   display: flex;
   flex-direction: column;
   padding-top: 20px;
-  background: #ffffff;
+  background: ${({ theme }) => theme.bg1};
+
   .menu-list {
     flex: 1;
-    .menu-item {
-      padding: 10px 5px 10px 15px;
-      display: flex;
-      align-items: center;
-      img {
-        width: 20px;
-        height: 20px;
-        margin-right: 10px;
-      }
-    }
   }
+
   .menu-footer {
     height: 100px;
     border-radius: 30px 30px 0 0;
+    background: #f2f0e8;
+    padding: 10px;
+
+    .theme-switch-btn {
+      cursor: pointer;
+      display: inline-block;
+    }
+
+    .language-switch-btn {
+      position: relative;
+      cursor: pointer;
+
+      .language-list {
+        width: 120px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        border-radius: 20px;
+        transform: translateY(-100%);
+        background: #c9c9c9;
+
+        .language-item {
+          padding: 10px;
+          background: #ffffff;
+        }
+      }
+    }
+  }
+`
+export const MenuItem = styled(NavLink)`
+  padding: 10px 5px 10px 15px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  img {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+  }
+  &.active {
     background: #f2f0e8;
   }
 `
